3b5cf7241335ef4b3f97df203afa06db7ab959df
TsubasaKatakiri <katakiritsubasa@gmail.com>
2022-12-03 20:55:44 +0300

    Added text highlighting in edit mode

--- src/components/NoteEditForm/NoteEditForm.tsx
+++ src/components/NoteEditForm/NoteEditForm.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, FormEvent, useRef, useState } from 'react';
 import { INote } from '../../models';
 import classes from './NoteEditForm.module.scss';
+import './highlightTextAssist.css';
 import parse from 'html-react-parser';
 import ContentEditable from 'react-contenteditable'
 import { extractTags, inlineTags } from '../../util/textFunctions';
@@ -13,14 +14,25 @@ interface NoteEditProps{
 const NoteEditForm = ({note, onSaveEdit} : NoteEditProps) => {
     const [title, setTitle] = useState<string | undefined>(note?.header);
     const [text, setText] = useState<string>(note.text);
+    const [underlayText, setUnderlayText] = useState<string>(textProcessing(note.text))
     const [tags, setTags] = useState<string>(note.tags ? inlineTags(note.tags) : '');
 
     const [textError, setTextError] = useState<string>('');
+
+    function textProcessing(text : string) : string{
+        const line = text.split(' ').map((word) => {
+            if(word.startsWith('#')) return `<span className='highlightedText'>${word}</span>`;
+            else return word;
+        }).join(' ');
+        console.log(line);
+        return line;
+    }
 
     const textInputHandler = (e: ChangeEvent<HTMLInputElement> | any) : void => {
         const textString = e.target.value;
         setTags(extractTags(textString));
         setText(textString);
+        setUnderlayText(textProcessing(textString));
     }
 
     const submitHandler = (e : FormEvent) : void => {
@@ -40,7 +52,12 @@ const NoteEditForm = ({note, onSaveEdit} : NoteEditProps) => {
     return (
         <form className={classes.form} onSubmit={submitHandler}>
             <input type='text' className={classes.form__input} placeholder={'Note Title'} value={title} onChange={e => setTitle(e.target.value)}/>
-            <textarea value={text} className={classes.form__textarea} onChange={textInputHandler} placeholder='Note Text'></textarea>
+            <div className={classes.form__highlightableTextarea}>
+                <textarea value={text} className={classes.form__textarea} onChange={textInputHandler} placeholder='Note Text'></textarea>
+                <div className={classes.form__highlightLayer}>
+                    {parse(underlayText)}
+                </div>
+            </div>
             {textError && <span className={classes.form__error}>{textError}</span>}
             <input type='text' className={classes.form__input} placeholder={'Note Tags'} value={tags} onChange={e => setTags(e.target.value)}/>
             <button className={classes.form__button}>Save Note</button>
